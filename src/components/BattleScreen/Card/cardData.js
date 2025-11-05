c401d33e81fc2dc89f965d1c12c24d651c14714d
createlsworklappy <justinecastillo7@gmail.com>
2025-11-05 15:34:43 +0900

    refactor(cardData): implement helper function to parse from deck folders, return all decks object with individual themed card objects

--- src/components/BattleScreen/Card/cardData.js
+++ src/components/BattleScreen/Card/cardData.js
@@ -1,12 +1,22 @@
-const allImageModules = import.meta.glob('/src/assets/tarot-cards/*.png', { eager: true, query: '?url', import: 'default' });
+const blackGoldModules = import.meta.glob('/src/assets/black-gold-cards/*.png', { eager: true, query: '?url', import: 'default' });
+const purpleChromeModules = import.meta.glob('/src/assets/purple-chrome-cards/*.png', { eager: true, query: '?url', import: 'default' });
 
-export const allCards = Object.entries(allImageModules).map(([path, imgUrl], index) => {
-  const fileName = path.split('/').pop();
-  const name = fileName.split('.')[0];
+function processCardModules(moduleObject) {
+  return Object.entries(moduleObject).map(([path, imgUrl], index) => {
+    const fileName = path.split('/').pop();
+    const name = fileName.split('.')[0];
 
-  return {
-    id: index + 1,
-    name: name,
-    img: imgUrl,
-  };
-});
+    return {
+      id: index + 1,
+      name: name,
+      img: imgUrl,
+    };
+  });
+}
+
+const allDecks = {
+  blackGold: processCardModules(blackGoldModules),
+  purpleChrome: processCardModules(purpleChromeModules),
+};
+
+export default allDecks;
